473cc5fb2247b9c4124f98a792e3ccdca77be6f0
jacksonmonteiro <jackson.monteiro@gfxconsultoria.com>
2023-10-16 11:01:57 -0300

    Login Screen Design

--- app/src/main/java/com/example/gymmanager/MainActivity.kt
+++ app/src/main/java/com/example/gymmanager/MainActivity.kt
@@ -1,11 +1,101 @@
 package com.example.gymmanager
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.compose.GymManagerTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            GymManagerTheme() {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Screen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Screen() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.background),
+            contentDescription = "Background",
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.fillMaxSize()
+        )
+
+        Column(
+            modifier = Modifier
+                .padding(all = 12.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center
+        ) {
+            WelcomeMessage()
+            LoginButton()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ScreenPreview() {
+    GymManagerTheme {
+        Screen()
+    }
+}
+
+/* Components */
+
+@Composable
+fun WelcomeMessage() {
+    Text(
+        "Seja Bem-Vindo!",
+        fontSize = 32.sp,
+        color = Color.White,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center,
+        modifier = Modifier.fillMaxWidth()
+    );
+}
+
+@Composable
+fun LoginButton() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        ElevatedButton(
+            onClick = { },
+            modifier = Modifier.weight(1f),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFD66A40),
+            )
+        )
+        {
+            Text("Entrar");
+        }
     }
 }
